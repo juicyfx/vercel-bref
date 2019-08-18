fae4d344ccc7e489d56c0089cc50c436d661a2c2
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-18 12:52:33 +0200

    Examples: added symfony demo

---
+++ examples/bref-symfony/src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('hello.html.twig');
+    }
+}
