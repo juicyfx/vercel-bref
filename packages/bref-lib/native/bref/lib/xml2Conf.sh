1f1d991b4ce00610644b6dde77c3a8a248abdd1c
Milan Felix Šulc <rkfelix@gmail.com>
2019-09-10 15:52:08 +0200

    [bref-lib] don't include dist, native, node_modules

--- packages/bref-lib/native/bref/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/opt/bref/lib"
-XML2_LIBS="-lxml2 -L/opt/bref/lib -lz     -lm "
-XML2_INCLUDEDIR="-I/opt/bref/include/libxml2"
-MODULE_VERSION="xml2-2.9.8"
-
