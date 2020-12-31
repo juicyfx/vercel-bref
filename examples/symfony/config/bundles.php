98972804c6b5a64a67fc090a3394496939f8e8ac
Milan Felix Šulc <rkfelix@gmail.com>
2020-12-31 17:49:33 +0100

    Example: drop symfony example

--- examples/symfony/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-];
